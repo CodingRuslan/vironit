36aa90c20fb36521c8ecc042641963830eee80f7
CodingRuslan <orion.hola@gmail.com>
2019-07-10 11:12:45 +0300

    feat: add some functional

--- inherit.js
+++ inherit.js
@@ -2,13 +2,34 @@ function B() {
 	this.counter = 0;
 }
 
-
-// function inherit(parrent, obj) {
-//   constructor() = {
-//     this
+// function F() {
+//   B.apply(this, arguments);
+//   this.constructor = function() {
+//     this.name = "foo";
 //   }
-//   otherMethod
+//   this.getCounter = function() {
+//     return this.counter;
+//   }
 // }
+
+// F.prototype = Object.create(B.prototype)
+// F.prototype.constructor = F;
+
+function inherit(parrentObj, childObj) {
+  
+
+  const newObj = new Object();
+  // const key = Object.keys(childObj);
+  for(key in childObj) {
+    newObj.prototype[key] = childObj[key];
+    // this.key = childObj[key]
+  }
+
+  parrentObj.apply(newObj, arguments);
+  return newObj;
+  // childObj.prototype = Object.create(parrentObj.prototype)
+  // childObj.prototype.constructor = childObj;
+}
  
 const F = inherit(B, {
   constructor: function() {
