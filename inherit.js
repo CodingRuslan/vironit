ee021bfec83452def1e9bf5b61cde5a435d7ef9a
CodingRuslan <orion.hola@gmail.com>
2019-07-11 12:15:14 +0300

    feat: add prototype inheritance

--- inherit.js
+++ inherit.js
@@ -2,16 +2,18 @@ function B() {
 	this.counter = 0;
 }
 
-function inherit(parrentObj, childObj) {
-  childObj.prototype = Object.create(parrentObj.prototype)
-  childObj.prototype.constructor = childObj; 
-
+function inherit(parrentObj, proto) {
+  const { constructor, ...prototype} = proto;
   function newConstructor() {
     parrentObj.apply(this, arguments);
-    for(let key in childObj) {
-      this[key] = childObj[key];
-    }
+    constructor.apply(this, arguments);
   }
+
+  newConstructor.prototype = Object.create(parrentObj.prototype)
+  newConstructor.prototype.constructor = newConstructor;
+
+  newConstructor.prototype = Object.keys(prototype).reduce((currentPrototype, method) => 
+   Object.assign({}, currentPrototype, { [method]: prototype[method] }), newConstructor.prototype);
 
   return newConstructor
 }
